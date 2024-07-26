a894b936fb2aee33dce8836204f0b671ba2249a1
EdgarMtzSndvl <desarrollo_01@dxt.com.mx>
2024-07-26 09:21:06 -0600

    quill implementado

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './button/Button';
 import InputText from './input/InputText';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import InputDateTime from './input/InputDateTime';
 import InputFile from './input/InputFile';
 import Card from './card/Card';
 import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import Editor from './quill/Editor';
-const Delta = Quill.import('delta');
+import Delta from 'quill-delta';
 
 function App() {
 
@@ -35,12 +35,22 @@ function App() {
   const [lastChange, setLastChange] = useState();
   const [readOnly, setReadOnly] = useState(false);
 
+
   const quillRef = useRef();
+
+  const handleButtonClick = () => {
+    const content = quillRef.current.getContents();
+    const jsonString = JSON.stringify(content);
+    console.log(content);
+  };
+
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+
         <section >
           <h2>Buttons</h2>
           <Button
@@ -66,7 +76,7 @@ function App() {
             outline={true}
           />
         </section>
-        
+
         <section >
           <h2>Text Inputs</h2>
           <InputText
@@ -85,11 +95,11 @@ function App() {
             outline={true}
           />&nbsp;
         </section>
-        
+
         <section >
           <h2>DateTime Inputs</h2>
           <InputDateTime
-            initialDate='2024-10-01T10:09' 
+            initialDate='2024-10-01T10:09'
             color="primary"
             onChange={handleDateTimeChange}
             showTime={true}
@@ -100,27 +110,27 @@ function App() {
             onChange={handleDateTimeChange}
           />&nbsp;
         </section>
-        
+
         <section>
           <h2>File Input</h2>
           <InputFile
-        onFileChange={handleFileChange}
-        color="danger"
-      />
+            onFileChange={handleFileChange}
+            color="danger"
+          />
         </section>
-        
+
         <section >
           <h2>Cards</h2>
           <Card
             header={<h2 className="text-2xl font-bold">tituloo</h2>}
             content={<p>aaaaaaaaaaaaaaaaa</p>}
-            footer={ <Button
+            footer={<Button
               text="Primary Button"
               color="primary"
               onClick={handleClick}
             />}
             color="primary"
-          /><br/>
+          /><br />
           <Card
             header={<h2 className="text-2xl font-bold">esto es un header</h2>}
             content={<p>contenidoooo</p>}
@@ -132,20 +142,20 @@ function App() {
               onChange={handleInputChange}
             />}
             color="secondary"
-          /><br/>
+          /><br />
           <Card
             header={<h2 className="text-2xl font-bold">aaaaaaaaaaa</h2>}
             content={<p>bbbbbbbbbb</p>}
             footer={
               <InputFile
-              onFileChange={handleFileChange}
-              color="secondary"/>
+                onFileChange={handleFileChange}
+                color="secondary" />
             }
             color="secondary"
-          /><br/>
+          /><br />
         </section>
-        <section>
-        <Editor
+        <section className='bg-white text-black'>
+      <Editor
         ref={quillRef}
         readOnly={readOnly}
         defaultValue={new Delta()
@@ -156,10 +166,19 @@ function App() {
           .insert(' ')
           .insert('content', { underline: true })
           .insert('\n')}
-        onSelectionChange={setRange}
-        onTextChange={setLastChange}
+        onChangeSelection={setRange}
+        onChange={setLastChange}
       />
-        </section>
+      <div className="controls">
+        <button
+          className="controls-right"
+          type="button"
+          onClick={handleButtonClick}
+        >
+          Convert to JSON
+        </button>
+      </div>
+    </section>
 
         <a
           className="App-link"
